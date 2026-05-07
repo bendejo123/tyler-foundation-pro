e7743dd91d575803e0c730a0740bb421139450b4
be <ben@bes-MacBook-Pro.local>
2026-05-07 15:31:13 -0700

    Fix site URL in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,22 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://tylerfoundationpros.com',
+  site: 'https://tylerfoundationpro.com',
   integrations: [
     tailwind(),
     sitemap({
       customPages: [
-        'https://tylerfoundationpros.com/',
-        'https://tylerfoundationpros.com/slab-foundation-repair/',
-        'https://tylerfoundationpros.com/pier-beam-repair/',
-        'https://tylerfoundationpros.com/house-leveling/',
-        'https://tylerfoundationpros.com/commercial-foundation-repair/',
-        'https://tylerfoundationpros.com/lindale-tx/',
-        'https://tylerfoundationpros.com/whitehouse-tx/',
-        'https://tylerfoundationpros.com/bullard-tx/',
-        'https://tylerfoundationpros.com/flint-tx/',
-        'https://tylerfoundationpros.com/hideaway-tx/',
-        'https://tylerfoundationpros.com/chandler-tx/',
+        'https://tylerfoundationpro.com/',
+        'https://tylerfoundationpro.com/slab-foundation-repair/',
+        'https://tylerfoundationpro.com/pier-beam-repair/',
+        'https://tylerfoundationpro.com/house-leveling/',
+        'https://tylerfoundationpro.com/commercial-foundation-repair/',
+        'https://tylerfoundationpro.com/lindale-tx/',
+        'https://tylerfoundationpro.com/whitehouse-tx/',
+        'https://tylerfoundationpro.com/bullard-tx/',
+        'https://tylerfoundationpro.com/flint-tx/',
+        'https://tylerfoundationpro.com/hideaway-tx/',
+        'https://tylerfoundationpro.com/chandler-tx/',
       ],
     }),
   ],
