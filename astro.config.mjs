663550943139940ef1e9b3d11b5a38b2dcffecd7
be <ben@bes-MacBook-Pro.local>
2026-05-07 15:33:02 -0700

    Remove customPages from sitemap to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,6 @@ export default defineConfig({
   site: 'https://tylerfoundationpro.com',
   integrations: [
     tailwind(),
-    sitemap({
-      customPages: [
-        'https://tylerfoundationpro.com/',
-        'https://tylerfoundationpro.com/slab-foundation-repair/',
-        'https://tylerfoundationpro.com/pier-beam-repair/',
-        'https://tylerfoundationpro.com/house-leveling/',
-        'https://tylerfoundationpro.com/commercial-foundation-repair/',
-        'https://tylerfoundationpro.com/lindale-tx/',
-        'https://tylerfoundationpro.com/whitehouse-tx/',
-        'https://tylerfoundationpro.com/bullard-tx/',
-        'https://tylerfoundationpro.com/flint-tx/',
-        'https://tylerfoundationpro.com/hideaway-tx/',
-        'https://tylerfoundationpro.com/chandler-tx/',
-      ],
-    }),
+    sitemap(),
   ],
 });
